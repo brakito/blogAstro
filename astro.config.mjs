2aeca205d7f10c9c7392a0ec2b90d76cc8c7d22d
Brakito <arangobrmst@gmail.com>
2024-02-14 18:30:05 -0500

    added a hydrated component as island with preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://arkane-blog.netlify.app/"
+  site: "https://arkane-blog.netlify.app/",
+  integrations: [preact()]
 });
